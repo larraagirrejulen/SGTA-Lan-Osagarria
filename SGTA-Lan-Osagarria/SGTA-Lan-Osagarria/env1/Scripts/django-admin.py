c87af107f144ba62df0d2341f01c9dbd28f6e8de
Julen <larraagirrejulen@gmail.com>
2022-05-18 10:49:27 +0200

    docker django

---
+++ SGTA-Lan-Osagarria/SGTA-Lan-Osagarria/env1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\UNI\SGTA\Lan-Osagarria\SGTA-Lan-Osagarria\SGTA-Lan-Osagarria\env1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
